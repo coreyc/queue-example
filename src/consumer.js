ad5e2d0f01032f0a58b9a606a27cc45ee6a66204
coreyc <ctc272@nyu.edu>
2019-03-21 07:31:09 -0500

    swapped rpoplpush for brpoplpush

--- src/consumer.js
+++ src/consumer.js
@@ -5,7 +5,7 @@ const client = redis.createClient()
 const {pushToQueue} = require('./producer')
 const {insert} = require('./db')
 
-const rpoplpush = promisify(client.rpoplpush).bind(client)
+const brpoplpush = promisify(client.brpoplpush).bind(client)
 const lrem = promisify(client.lrem).bind(client)
 const lrange = promisify(client.lrange).bind(client)
 
@@ -19,9 +19,6 @@ client.on('connect', () => {
 client.on('error', err => {
   console.log(`Redis client connection error: ${err}`)
 })
-
-// TODO: is polling method the best way? try blocking
-// TODO: queue diagram (how it fills up), worker on different server
 
 // consumer / worker
 // generic function to handle multiple different queues
@@ -51,7 +48,7 @@ const checkStales = async (queueName, timeout) => {
 const getWork = async (workQueue, processingQueue) => {
   try {
     // this removes from work/todo queue
-    return await rpoplpush(workQueue, processingQueue)
+    return await brpoplpush(workQueue, processingQueue, 120) // 120 seconds, not ms
   } catch(e) {
     throw new Error(e)
   }
@@ -78,31 +75,19 @@ const getQueueLength = async (queueName) => {
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-const run = (async() => {  
+const run = (async() => {
   // first, check stale items in processing queue
   await checkStales(WORK_QUEUE, 120000) // 2 minute stale time
 
-  // next, do work stuff
-  let workQueueHasWork = await getQueueLength(WORK_QUEUE)
-  while (workQueueHasWork.length) {
-    // not necessary, just to be able to see the console logging more easily
-    await sleep(500)
+  // this was screwing up brpoplpush
+  // await sleep(5000)
 
-    let workItem
+  let workItem
 
-    try {
-      workItem = await getWork(WORK_QUEUE, PROCESSING_QUEUE)
-    } catch(e) {
-      console.error(`Error getting work item from ${PROCESSING_QUEUE} queue: ${e}`)
-    }
-
-    try {
-      await doWork(workItem, PROCESSING_QUEUE)
-      console.log(`completed work item: ${workItem}`)
-    } catch(e) {
-      console.error(`Error doing work from ${PROCESSING_QUEUE} queue: ${e}`)
-    }
-    
-    queueHasWork = await getQueueLength(WORK_QUEUE)
-  }
+  try {
+    workItem = await getWork(WORK_QUEUE, PROCESSING_QUEUE)
+    await doWork(workItem, PROCESSING_QUEUE)
+  } catch(e) {
+    console.error(`Error processing work item from ${PROCESSING_QUEUE} queue: ${e}`)
+  }    
 })()
